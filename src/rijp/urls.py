234565416f45f44c7289e7816d8101ae6f7c4a9c
Jarek Bienkowski <jaroslaw.bienkowski@hotmail.com>
2018-06-12 15:48:51 +0200

    Added some css, js and imgs * Bootstrap * jQuery * Bulma

--- src/rijp/urls.py
+++ src/rijp/urls.py
@@ -3,12 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from rijp_portal import views as rijp_views
+
 CACHE_TIME_SHORT = int(getattr(settings, "CACHE_TIME_SHORT", 0))
 CACHE_TIME_MEDIUM = int(getattr(settings, "CACHE_TIME_MEDIUM", 0))
 CACHE_TIME_LONG = int(getattr(settings, "CACHE_TIME_LONG", 0))
 SB_URL_BASE = getattr(settings, "URL_BASE", "")
 
 urlpatterns = [
+    path('', rijp_views.IndexListView.as_view(), name='index'),
     path('admin/', admin.site.urls),
 ]
 
--- src/rijp_portal/views.py
+++ src/rijp_portal/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+class IndexListView(ListView):
+    model = None
+    context_object_name = 'ctx'
+    template_name = 'base.html'
+
+    def get_queryset(self):
+        return None
